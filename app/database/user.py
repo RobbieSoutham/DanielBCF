971de0fa9313937d840e1c9a3ac2a920e5ed0d6e
Luke Southam <luke@devthe.com>
2018-12-09 09:12:14 +0000

    Updated database.user

--- app/database/user.py
+++ app/database/user.py
@@ -6,13 +6,13 @@ from bcrypt import hashpw, gensalt
 
 from . import Database
 
-class User(Database):
+class User():
     @classmethod
     def new_user(cls, **kwargs):
         kwargs['first_name'] = kwargs['first_name'].title()
         kwargs['surname'] = kwargs['surname'].title()
         kwargs['password'] = hashpw(kwargs['password'], gensalt())
-        cls.insert_into(
+        Database.insert_into(
             "Users",
             ["email", "first_name", "surname", "password"],
             kwargs
@@ -20,4 +20,4 @@ class User(Database):
     
     @classmethod
     def delete_user(cls, email):
-        cls.delete("Users", "email", email)
+        Database.delete("Users", "email", email)
