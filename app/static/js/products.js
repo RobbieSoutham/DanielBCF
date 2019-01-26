0512ca065e96604a0728db695df7f07d6c80fc04
Robbie Southam <rjsoutham@gmail.com>
2019-01-26 10:00:00 +0000

    Further products functionality

--- app/static/js/products.js
+++ app/static/js/products.js
@@ -9,14 +9,20 @@ $(function() {
             var html = '';
             status = "";
             $.each(data, function (i, item) {
-                    
-                    html += "<tr><td>" + item.name + "</td>center><center><td> " + item.id + "</td></center><td><center>" + item.order_qty + "</td></center><td><center><a>" + item.cossh + "</a></td></center><center><td><img src='" + $SCRIPT_ROOT + "/static/open-iconic-master/svg/account-login.svg' alt='account login'></td></center>";
+                    btn_html = "<center><button type='btn' id='edit' onclick=\"edit(" + item.id + ", '" + item.name + "', " + item.order_qty + ", '" + item.cossh + "');\"><img src='" + $SCRIPT_ROOT + "/static/open-iconic-master/svg/pencil.svg' alt='pencil'></button><center>";
+                    html += "<tr><td>" + item.name + "</td>center><center><td> " + item.id + "</td></center><td><center>" + item.order_qty + "</td></center><td><center><a href=" + item.cossh + ">Link</a></td></center><center><td>" + btn_html + "</td></center>";
                 
             });
-            console.log(html)
+            console.log(btn_html)
             //Append every product item to the table          
             $('#products tbody').append(html);
         }
         
     });
 });
+
+function edit(id, name, order_qty, cossh){
+    $('#modal-title').text(name);
+    $('#modal_edit').modal('show');
+    
+}
