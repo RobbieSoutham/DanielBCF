536335771284bf483a455178582b64d02bbabba1
Robbie Southam <rjsoutham@gmail.com>
2019-02-22 15:21:20 +0000

    a

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+import yaml
+
+PATH = os.path.dirname(__file__)
+CONFIG = PATH + "/configs/{}.yaml".format(
+    os.environ.get('FLASK_ENV', "development")
+)
+
+# Creates our Flask application.
+app = Flask(__name__)
+
+with open(CONFIG, 'r') as config: 
+    app.config.update(
+        **yaml.load(config)
+    )
+
+from app import views, errors, commands
