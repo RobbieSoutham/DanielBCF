35c2332145e3a4515b672f2b2f37d4f21e0d4798
RobbieSoutham <45611649+RobbieSoutham@users.noreply.github.com>
2019-03-15 16:14:48 +0000

    Update index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,8 +1,10 @@
-$("#remember_me").change(function() {
+$(function() {
+    $("#remember_me").change(function() {
     if(this.checked) {
         $("#remember_me").val("")
     }
     else{
       $("#remember_me").val("1")
     }
+});
 });
