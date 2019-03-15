9595991ae041500b5004ebd334ed341e6ee641bf
RobbieSoutham <45611649+RobbieSoutham@users.noreply.github.com>
2019-03-15 16:18:16 +0000

    Update views.py

--- app/views.py
+++ app/views.py
@@ -65,6 +65,7 @@ def login():
             )
             if user:
                 #If user exists log them in
+                print(form.remember_me.data)
                 login_user(user, remember=form.remember_me.data)
                 flash("Logged in successfully.", "success")
                 return redirect(url_for("stock"))
