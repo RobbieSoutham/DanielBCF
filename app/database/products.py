ba83d2ab3f9d62cee2c95ca2a5f56c16cfdd173d
RobbieSoutham <45611649+RobbieSoutham@users.noreply.github.com>
2019-03-17 14:15:44 +0000

    Update products.py

--- app/database/products.py
+++ app/database/products.py
@@ -20,7 +20,7 @@ class Product():
 
     @classmethod
     def new_product(cls, **kwargs):
-        kwargs['name'] = kwargs['name'].title()
+        kwargs["name"] = kwargs["name"].title()
         Database.insert_into(
             cls._tablename,
             ["id", "name", "order_qty", "cossh"],
@@ -32,8 +32,8 @@ class Product():
 
         for site in sites:
             #Add a new record for each site to the stock table for the site
-            kwargs['site_id'] = site[0]
-            kwargs['product_id'] = kwargs['id']
+            kwargs["site_id"] = site[0]
+            kwargs["product_id"] = kwargs["id"]
             Database.insert_into("Stock", ["product_id", "site_id"],
             kwargs
             )
@@ -51,16 +51,16 @@ class Product():
 
         #Return products data as JSON
         for result in results:
-                content = {'id': result[0], 'name': result[1], 'order_qty': result[2], "cossh": result[3]}
+                content = {"id": result[0], "name": result[1], "order_qty": result[2], "cossh": result[3]}
                 data.append(content)
                 content = {}
         return jsonify(data)
     
     @classmethod
     def update_product(cls, **kwargs):
-        if kwargs['previous_id'] != kwargs['name']:
-            Database.update(cls._tablename, "id", kwargs['id'], "id", kwargs['previous_id'])
+        if kwargs["previous_id"] != kwargs["name"]:
+            Database.update(cls._tablename, "id", kwargs['id'], "id", kwargs["previous_id"])
             
-        Database.update(cls._tablename, "name", kwargs['name'], "id", kwargs['id'])
-        Database.update(cls._tablename, "cossh", kwargs['cossh'], "id", kwargs['id'])
-        Database.update(cls._tablename, "order_qty", kwargs['order_qty'], "id", kwargs['id'])
+        Database.update(cls._tablename, "name", kwargs["name"], "id", kwargs["id"])
+        Database.update(cls._tablename, "cossh", kwargs['cossh'], "id", kwargs["id"])
+        Database.update(cls._tablename, "order_qty", kwargs["order_qty"], "id", kwargs["id"])
