8dee6389c92899bb79f83f5df2874625da2f8b1b
RobbieSoutham <45611649+RobbieSoutham@users.noreply.github.com>
2019-03-17 14:00:33 +0000

    Update cossh.js

--- app/static/js/cossh.js
+++ app/static/js/cossh.js
@@ -2,6 +2,10 @@ function ajax_insert(item){
     html = "<tr><td>" + item.name + "</td><center><center><td><a href='" + item.cossh + "'>" + item.cossh + "</a></td></center>";
     return html;
 }
+
 $(function(){
     ajax_return("product_list");
 });
+
+ajax_follow(){
+}
