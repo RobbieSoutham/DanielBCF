3404bc5e1396eec73b5c60b7e631df201c8a36d9
RobbieSoutham <45611649+RobbieSoutham@users.noreply.github.com>
2019-03-24 19:08:26 +0000

    Update sites.js

--- app/static/js/sites.js
+++ app/static/js/sites.js
@@ -1,7 +1,7 @@
 function ajax_insert(item){
-    btn1 = "<button type='button' class='buttons' id='edit' onclick='edit(\"" + item.name + "\", \"" + item.address + "\");'><img src='" + $SCRIPT_ROOT + "/static/open-iconic-master/svg/pencil.svg' alt='pencil'></button>"; 
-    btn2 = "<button class='buttons' id='delete' onclick='delete_site(\"" + item.name + "\");'><img src='" + $SCRIPT_ROOT + "/static/open-iconic-master/svg/x.svg' alt='x'></button>";
-    html = "<tr><td class='name'>" + item.name + "</td><center><center><td> " + item.address + "</td></center><td><center>" + btn1 + btn2 + "</center></td>";
+    var btn1 = "<button type='button' class='buttons' id='edit' onclick='edit(\"" + item.name + "\", \"" + item.address + "\");'><img src='" + $SCRIPT_ROOT + "/static/open-iconic-master/svg/pencil.svg' alt='pencil'></button>"; 
+    var btn2 = "<button class='buttons' id='delete' onclick='delete_site(\"" + item.name + "\");'><img src='" + $SCRIPT_ROOT + "/static/open-iconic-master/svg/x.svg' alt='x'></button>";
+    var html = "<tr><td class='name'>" + item.name + "</td><center><center><td> " + item.address + "</td></center><td><center>" + btn1 + btn2 + "</center></td>";
     return html;
 }
 
